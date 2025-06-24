4eec81836fe3667b20ba49139c1fe4b883cdf671
unknown <hei.teddy.2@gmail.com>
2025-06-24 08:59:58 +0300

    Apply code format

--- src/main/java/school/hei/arith/endpoint/AdditionController.java
+++ src/main/java/school/hei/arith/endpoint/AdditionController.java
@@ -1,2 +1,13 @@
-package school.hei.arith.endpoint;public class AdditionController {
+package school.hei.arith.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+  @GetMapping("/add")
+  public int add(@RequestParam int a, @RequestParam int b) {
+    return a + b;
+  }
 }
--- src/test/java/school/hei/arith/endpoint/AdditionControllerTest.java
+++ src/test/java/school/hei/arith/endpoint/AdditionControllerTest.java
@@ -1,4 +1,14 @@
+package school.hei.arith.endpoint;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class AdditionControllerTest {
-  
+  private final AdditionController subject = new AdditionController();
+
+  @Test
+  void testAdd() {
+    assertEquals(3, subject.add(1, 2));
+  }
 }
